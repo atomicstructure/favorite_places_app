c3a8eefeebe65c1873dae231e43075da2e21715a
Ogundare Olusesi Oluwafemi <ogundarearold@gmail.com>
2023-11-23 11:48:53 +0100

    Finished with the model of my place

--- lib/main.dart
+++ lib/main.dart
@@ -32,8 +32,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Great Places',
       theme: theme,
+      home: ...,
     );
   }
 }
