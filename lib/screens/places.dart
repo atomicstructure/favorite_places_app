c6bec7703ec4d116aca52eb83a2cca267f75ad6d
Ogundare Olusesi Oluwafemi <ogundarearnold@gmail.com>
2023-12-01 11:38:47 +0100

    Added a Future to the db

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -4,30 +4,52 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class PlacesScreen extends ConsumerWidget {
+class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return _PlacesScreenState();
+  }
+}
+
+class _PlacesScreenState extends ConsumerState<PlacesScreen> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final userPlaces = ref.watch(userPlacesProvider);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Places'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              context.goNamed('addplace');
-            },
-            icon: const Icon(Icons.add),
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: PlacesList(
-          places: userPlaces,
+        appBar: AppBar(
+          title: const Text('Places'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                context.goNamed('addplace');
+              },
+              icon: const Icon(Icons.add),
+            ),
+          ],
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FutureBuilder(
+            future: _placesFuture,
+            builder: (context, snapshot) =>
+                snapshot.connectionState == ConnectionState.waiting
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : PlacesList(
+                        places: userPlaces,
+                      ),
+          ),
+        ));
   }
 }
